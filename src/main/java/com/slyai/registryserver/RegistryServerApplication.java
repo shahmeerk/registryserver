7f29eca0330f32b2ed1b37a13ad168c7b18d5eb2
Shahmeer Khan <shahmeer.a.khan@gmail.com>
2023-08-22 01:07:22 +0100

    test webhook

--- src/main/java/com/slyai/registryserver/RegistryServerApplication.java
+++ src/main/java/com/slyai/registryserver/RegistryServerApplication.java
@@ -11,6 +11,4 @@ public class RegistryServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(RegistryServerApplication.class, args);
     }
-    //TEST
-
 }
---
+++ src/main/java/com/slyai/registryserver/Test.java
@@ -0,0 +1,4 @@
+package com.slyai.registryserver;
+
+public class Test {
+}
